a851bb483622af5d3760362abdcebc4b380cf7a3
sylvano <sylvano.verkuyl@nugtr.nl>
2021-04-26 11:57:52 +0200

    - Altered some structure in the module pattern. - Added a handler to check if the specified element is hovered over to prevent page scrolling. - Added possibility to init the widget with an element. - Compacted styling. - Altered the way the timeout was handled while scrolling. - Added some extra checks for nulls and undefined

--- ArithmaSlide.js
+++ ArithmaSlide.js
@@ -1,135 +1,118 @@
-const ArithmaSlideWidget = (function() {
+const ArithmaSlideWidget = (function () {
     let allPages;
+    const ConfigMap = {}
 
-    let ConfigMap = {
-
-    }
-
-    const _init = function (){
-        if (!ConfigMap.hasOwnProperty('cssAnimationOut')||
-        !ConfigMap.hasOwnProperty('cssAnimationIn')){
+    const _init = function (element) {
+        if (!ConfigMap.hasOwnProperty('cssAnimationOut') ||
+            !ConfigMap.hasOwnProperty('cssAnimationIn')) {
             generateStandardCssAnimation();
         }
-
-        if (!ConfigMap.hasOwnProperty('container')){
+        if (element === null || element === undefined) {
             document.body.style.overflow = 'hidden';
             allPages = document.querySelectorAll('.ArithmaSlide');
             ArithmaSlideWidget.ScrollHandler.init(document.body);
         } else {
-            let container = document.getElementById(ConfigMap.container)
+            let container = document.getElementById(element)
             container.style.overflow = 'hidden';
             allPages = container.querySelectorAll('.ArithmaSlide');
             ArithmaSlideWidget.ScrollHandler.init(container);
         }
     }
 
-    function generateStandardCssAnimation(){
-        let keyframes = '@keyframes fadeOut {\n' +
-            '    0%{\n' +
-            '        margin-top: 0;\n' +
-            '    }\n' +
-            '    100%{\n' +
-            '        margin-top: -100vh;\n' +
-            '    }\n' +
-            '}\n' +
-            '\n' +
-            '@keyframes fadeIn {\n' +
-            '    0%{\n' +
-            '        margin-top: -100vh;\n' +
-            '    }\n' +
-            '    100%{\n' +
-            '        margin-top: 0;\n' +
-            '    }\n' +
-            '}';
-
+    const generateStandardCssAnimation = () => {
         let style = document.createElement('style');
-        style.innerHTML = '\n.fullSlideOut{animation: fadeOut 0.8s forwards}' +
-            '\n.fullSlideIn{animation: fadeIn 0.8s forwards}\n'+
-            keyframes;
+        style.innerHTML = '.fullSlideOut{animation: fadeOut 0.8s forwards}.fullSlideIn{animation: fadeIn 0.8s forwards}' +
+            '@keyframes fadeOut {0%{margin-top: 0;}100%{margin-top: -100vh;}}@keyframes fadeIn {0%{margin-top: -100vh;}100%{margin-top: 0;}}';
 
-        ConfigMap.cssAnimationOut ='fullSlideOut';
-        ConfigMap.cssAnimationIn ='fullSlideIn';
+        ConfigMap.cssAnimationOut = 'fullSlideOut';
+        ConfigMap.cssAnimationIn = 'fullSlideIn';
 
         document.getElementsByTagName('head')[0].appendChild(style);
     }
 
-    const _getAllPages = function (){
+    const _getAllPages = function () {
         return allPages;
     }
 
-    return {
-        init: _init,
-        getAllPages:  _getAllPages,
-        config: ConfigMap
-    }
-}());
 
+    const ScrollHandler = (function () {
+        let isMoving = false;
+        let isActive = true;
+        let pageIndex = 0;
 
-
-////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////
-
-
-
-ArithmaSlideWidget.ScrollHandler = (function() {
-    let isMoving = false;
-    let pageIndex = 0;
-
-    const _init = function (parent){
-        parent.addEventListener("wheel", MouseWheelHandler);
-        function MouseWheelHandler(event) {
-
-            if (isMoving) return;
-            isMoving = true;
-            setTimeout(function() {
-                isMoving=false;
-            },2000);
-
-            if (event.deltaY < 0)
-            {
-                slidePageUp();
+        const _init = function (parent) {
+            parent.addEventListener("wheel", MouseWheelHandler, {passive: false});
+            if(parent !== document.body) {
+                parent.onmouseover = _mouseOverHandler;
+                parent.onmouseout = _mouseOutHandler;
             }
-            else if (event.deltaY > 0)
-            {
+        }
+
+        const _mouseOverHandler = function(event) {
+            event.preventDefault();
+            isActive = true;
+        }
+
+        const _mouseOutHandler = function() {
+            isActive = false;
+        }
+
+        const MouseWheelHandler = function (event) {
+            if (isMoving || !isActive) return;
+            event.preventDefault();
+            isMoving = true;
+
+            window.setTimeout(() => isMoving = false, 500);
+
+            if (event.deltaY < 0) {
+                slidePageUp();
+            } else if (event.deltaY > 0) {
                 slidePageDown();
             }
-            if (ArithmaSlideWidget.config.hasOwnProperty('pageIndexCallback')){
+            if (ArithmaSlideWidget.config.hasOwnProperty('pageIndexCallback')) {
                 ArithmaSlideWidget.config.pageIndexCallback(pageIndex);
             }
         }
-    }
+        //TODO: hoeveelheid wat gescrolled wordt aanpassen naarmate de grootte van de div
+        const slidePageDown = function () {
+            if (pageIndex < ArithmaSlideWidget.getAllPages().length - 1) {
+                let previousPage = ArithmaSlideWidget.getAllPages()[pageIndex];
+                pageIndex++;
 
-    const slidePageDown = function (){
-        if (pageIndex<ArithmaSlideWidget.getAllPages().length-1) {
-            let previousPage = ArithmaSlideWidget.getAllPages()[pageIndex];
-            pageIndex++;
+                if (previousPage.classList.contains(ArithmaSlideWidget.config.cssAnimationOut)
+                    && previousPage.classList.contains(ArithmaSlideWidget.config.cssAnimationIn)) {
+                    previousPage.classList.remove(ArithmaSlideWidget.config.cssAnimationOut);
+                    previousPage.classList.remove(ArithmaSlideWidget.config.cssAnimationIn);
+                }
 
-            if (previousPage.classList.contains(ArithmaSlideWidget.config.cssAnimationOut)
-                &&previousPage.classList.contains(ArithmaSlideWidget.config.cssAnimationIn)){
-                previousPage.classList.remove(ArithmaSlideWidget.config.cssAnimationOut);
-                previousPage.classList.remove(ArithmaSlideWidget.config.cssAnimationIn);
+                previousPage.classList.add(ArithmaSlideWidget.config.cssAnimationOut);
+            } else if (ArithmaSlideWidget.config.hasOwnProperty('noSlideDown')) {
+                ArithmaSlideWidget.config.noSlideDown();
             }
-
-            previousPage.classList.add(ArithmaSlideWidget.config.cssAnimationOut);
-        } else if (ArithmaSlideWidget.config.hasOwnProperty('noSlideDown')) {
-            ArithmaSlideWidget.config.noSlideDown();
         }
-    }
+        //TODO: hoeveelheid wat gescrolled wordt aanpassen naarmate de grootte van de div
+        const slidePageUp = function () {
+            if (pageIndex !== 0) {
+                pageIndex--;
+                let nextPage = ArithmaSlideWidget.getAllPages()[pageIndex];
 
-    const slidePageUp = function (){
-        if (pageIndex!==0) {
-            pageIndex--;
-            let nextPage = ArithmaSlideWidget.getAllPages()[pageIndex];
-
-            nextPage.classList.add(ArithmaSlideWidget.config.cssAnimationIn);
-        } else if (ArithmaSlideWidget.config.hasOwnProperty('noSlideUp')) {
-            ArithmaSlideWidget.config.noSlideUp();
+                nextPage.classList.add(ArithmaSlideWidget.config.cssAnimationIn);
+            } else if (ArithmaSlideWidget.config.hasOwnProperty('noSlideUp')) {
+                ArithmaSlideWidget.config.noSlideUp();
+            }
         }
-    }
+
+        return {
+            init: _init,
+            triggerUp: slidePageUp,
+            triggerDown: slidePageDown
+        }
+    }());
 
     return {
         init: _init,
-        triggerUp: slidePageUp,
-        triggerDown: slidePageDown
+        getAllPages: _getAllPages,
+        ScrollHandler: ScrollHandler,
+        config: ConfigMap
     }
 }());
